7cd1782b6cde016eaffa499f1ca80dcc5158aa7d
LauchieHarvey <lauchieharvey@gmail.com>
2020-05-08 15:19:31 +1000

    Some formatting and docstrings

--- a3.py
+++ a3.py
@@ -3,20 +3,28 @@
 
 class PokemonGame:
 	""" Model view controller class for the pokemon game."""
+	
 	def __init__(self):
+		""" Constructor method for the PokemonGame class"""
 		pass
 
 
 class BoardModel:
 	""" Class that represents the board/game state as an object"""
+
 	def __init__(self):
+		"""Constructor method for the BooardModel class"""
 		pass
 
+	def get_game(self):
+		"""Returns a representation of the game board as a string"""
+		return self._game_board
+
+
+
 class BoardView:
-	""" 
-		Handles the graphical user interface for the programme
-		mainly consisting of the game board (BoardModel instance).
-	"""		
+	""" Handles the graphical user interface for the programme"""
 
 	def __init__():
+		"""Constructor method for the BoardView class"""
 		pass
