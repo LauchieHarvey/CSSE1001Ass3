e241820476475e6c2f83248feb8128553c197a54
LauchieHarvey <lauchieharvey@gmail.com>
2020-05-08 12:55:51 +1000

    Defining classes and writing their docstrings

--- a3.py
+++ a3.py
@@ -1,1 +1,22 @@
-# Assignment 3, A graphical game of Pokemon
+# Assignment 3. Pokemon, Got 2 Find Them All!
+
+
+class PokemonGame:
+	""" Model view controller class for the pokemon game."""
+	def __init__(self):
+		pass
+
+
+class BoardModel:
+	""" Class that represents the board/game state as an object"""
+	def __init__(self):
+		pass
+
+class BoardView:
+	""" 
+		Handles the graphical user interface for the programme
+		mainly consisting of the game board (BoardModel instance).
+	"""		
+
+	def __init__():
+		pass
